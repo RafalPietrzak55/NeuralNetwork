b0d318fdf26c98b3bc2f375cad53f24998338dfe
RafalPietrzak55 <115399790+RafalPietrzak55@users.noreply.github.com>
2025-05-09 13:26:47 +0200

    mm

--- Program.cs
+++ Program.cs
@@ -4,9 +4,23 @@ namespace XOR_NeuralNetwork
 {
     class Program
     {
+        static double[][] inputs = new double[][]
+        {
+            new double[] {0, 0},
+            new double[] {0, 1},
+            new double[] {1, 0},
+            new double[] {1, 1}
+        };
+
+        static double[] targets = new double[] { 0, 1, 1, 0 };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Dane XOR:");
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                Console.WriteLine($"Wejście: [{inputs[i][0]}, {inputs[i][1]}], Oczekiwane wyjście: {targets[i]}");
+            }
         }
     }
 }
